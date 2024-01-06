e48d80cd662d3041ae9001dc665269d4c90930f1
mljxxx <704582254@qq.com>
2024-01-06 14:52:04 +0800

    update

--- mytv/mytv_checkout.js
+++ mytv/mytv_checkout.js
@@ -1,7 +1,9 @@
 var body = $response.body;
 var res = JSON.parse(body);
-res["breakpoints"] = []
-res["sponsored_ad_breakpoints"] = []
+delete res["breakpoints"]
+delete res["sponsored_ad_breakpoints"]
+delete res["ad_source_hybrid_priority"]
+delete res["ad_source_hybrid_timeout"]
 var body = JSON.stringify(res);
 console.log(body);
 $done(body);
