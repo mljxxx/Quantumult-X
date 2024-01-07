a490a903735cb2aa6456d21dc23ecae2910c0339
mljxxx <704582254@qq.com>
2024-01-07 21:37:01 +0800

    update

--- Notability/subscription.js
+++ Notability/subscription.js
@@ -1,29 +1,31 @@
 var response_body = JSON.parse($response.body);
-if(response_body["data"]["processAppleReceipt"]) {
-  response_body = {
-    "data": {
-      "processAppleReceipt": {
-        "error": 0,
-        "subscription": {
-          "productId": "com.gingerlabs.Notability.premium_subscription",
-          "originalTransactionId": "570001184068302",
-          "tier": "premium",
-          "refundedDate": null,
-          "refundedReason": null,
-          "isInBillingRetryPeriod": false,
-          "expirationDate": "2099-09-09T09:09:09.000Z",
-          "gracePeriodExpiresAt": null,
-          "overDeviceLimit": false,
-          "expirationIntent": null,
-          "__typename": "AppStoreSubscription",
-          "user": null,
-          "status": "canceled",
-          "originalPurchaseDate": "2022-09-09T09:09:09.000Z"
-        },
-        "__typename": "SubscriptionResult"
+if (response_body["data"]) {
+  if (response_body["data"]["processAppleReceipt"]) {
+    response_body = {
+      "data": {
+        "processAppleReceipt": {
+          "error": 0,
+          "subscription": {
+            "productId": "com.gingerlabs.Notability.premium_subscription",
+            "originalTransactionId": "570001184068302",
+            "tier": "premium",
+            "refundedDate": null,
+            "refundedReason": null,
+            "isInBillingRetryPeriod": false,
+            "expirationDate": "2099-09-09T09:09:09.000Z",
+            "gracePeriodExpiresAt": null,
+            "overDeviceLimit": false,
+            "expirationIntent": null,
+            "__typename": "AppStoreSubscription",
+            "user": null,
+            "status": "canceled",
+            "originalPurchaseDate": "2022-09-09T09:09:09.000Z"
+          },
+          "__typename": "SubscriptionResult"
+        }
       }
-    }
-  };
+    };
+  }
 }
 var body = JSON.stringify(response_body);
 console.log(body)
