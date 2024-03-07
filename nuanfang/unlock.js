b93d11cc69f6274b5d6b116dba7d9a13d044f274
mljxxx <704582254@qq.com>
2024-03-07 22:37:09 +0800

    update

--- nuanfang/unlock.js
+++ nuanfang/unlock.js
@@ -3,7 +3,9 @@ var res = JSON.parse(body);
 var rooms = res["rooms"]
 for(room in rooms) {
     room["memberOnly"] = false
-    console.log(room)
+    if(room["id"] === 11330404015) {
+        console.log(room)
+    }
 }
 var body = JSON.stringify(res);
 // console.log(body);
